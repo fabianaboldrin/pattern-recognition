f872014f5ddfd543c46419ba6d146ed25aee81a6
Fabiana Boldrin <bibacb.fb@gmail.com>
2018-06-24 14:17:11 -0300

    last version

--- Linear Regression/Applied Exercises/exercises_linear_regression_part5.R
+++ Linear Regression/Applied Exercises/exercises_linear_regression_part5.R
@@ -5,3 +5,10 @@ set.seed(1)
 x1=runif (100)
 x2=0.5*x1+rnorm (100)/10
 y=2+2*x1+0.3*x2+rnorm (100)
+'''
+The last line corresponds to creating a linear model in which y is
+a function of x1 and x2. Write out the form of the linear model.
+What are the regression coefficients?
+'''
+#Y=2+2X1+0.3X2+ϵ
+#β0=2,β1=2,β3=0.3
