d82f54e9fa288d4712e91e8a86eeadf8ee67da48
Fabiana Boldrin <bibacb.fb@gmail.com>
2018-06-24 14:18:21 -0300

    last version

--- Linear Regression/Applied Exercises/exercises_linear_regression_part5.R
+++ Linear Regression/Applied Exercises/exercises_linear_regression_part5.R
@@ -12,3 +12,19 @@ What are the regression coefficients?
 '''
 #Y=2+2X1+0.3X2+ϵ
 #β0=2,β1=2,β3=0.3
+'''
+Using this data, fit a least squares regression to predict y using
+x1 and x2. Describe the results obtained. What are βˆ0, βˆ1, and
+βˆ2? How do these relate to the true β0, β1, and β2? Can you
+reject the null hypothesis H0 : β1 = 0? How about the null
+hypothesis H0 : β2 = 0?
+'''
+lm.fit = lm(y~x1+x2)
+summary(lm.fit)
+'''
+ Now fit a least squares regression to predict y using only x1.
+Comment on your results. Can you reject the null hypothesis
+H0 : β1 = 0?
+'''
+lm.fit = lm(y~x1)
+summary(lm.fit)
